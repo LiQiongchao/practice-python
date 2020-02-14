3a3174fe4ddf181b2d60cd03b07428623129b0e0
qiongchaoli <qiongchaoli@haowaihao.com.cn>
2020-02-14 18:16:51 +0800

    忽略环境

---
+++ practice/tests/mainTest.py
@@ -0,0 +1,15 @@
+import unittest
+class test(unittest.TestCase):
+    def test_001(self):
+        print('001')
+    def test_002(self):
+        print('002')
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
